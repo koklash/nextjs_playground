01cf7ab39883ba11a5beba1b775eb3f4e96f149c
Niklas <niklas-moritz@live.de>
2022-12-13 16:40:32 +0100

    added player

---
+++ my-playground/pages/cloudflare/video.tsx
@@ -0,0 +1,18 @@
+function VideoPlayer() {
+  return (
+    <iframe
+				src="https://customer-m033z5x00ks6nunl.cloudflarestream.com/b236bde30eb07b9d01318940e5fc3eda/iframe?poster=https%3A%2F%2Fcustomer-m033z5x00ks6nunl.cloudflarestream.com%2Fb236bde30eb07b9d01318940e5fc3eda%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+				width="640"
+                height="360"
+                frameBorder="0"
+                scrolling="no"
+				allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+	>
+	</iframe>
+
+  )
+    
+  
+}
+
+export default VideoPlayer
